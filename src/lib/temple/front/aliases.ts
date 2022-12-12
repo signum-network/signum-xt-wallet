f5d58400b5434c42deda8d0512323098fa10e2f3
ohager <oliver@devbutze.com>
2022-12-12 11:56:32 -0300

    feat: added new SRC44 account name resolver

--- src/lib/temple/front/aliases.ts
+++ src/lib/temple/front/aliases.ts
@@ -8,6 +8,29 @@ interface AliasList {
   aliases: Alias[];
 }
 
+function tryGetAccountIdFromAliasContent(content: string): string | null {
+  let detectedAccount = '';
+  try {
+    // SRC44 standard
+    const json = JSON.parse(content);
+    detectedAccount = json.ac || '';
+  } catch (e: any) {
+    // legacy standard
+    const result = /^acct:(s|ts|burst)-(?<address>.*)@burst$/gm.exec(content);
+    if (result && result.groups) {
+      detectedAccount = 's-' + result.groups.address || '';
+    }
+  }
+
+  try {
+    return detectedAccount.startsWith('s-')
+      ? Address.fromReedSolomonAddress(detectedAccount).getNumericId()
+      : Address.fromNumericId(detectedAccount).getNumericId();
+  } catch (e: any) {
+    return null;
+  }
+}
+
 export function useSignumAliasResolver() {
   const signum = useSignum();
 
@@ -18,8 +41,17 @@ export function useSignumAliasResolver() {
         const accountId = address.getNumericId();
         const rs = address.getReedSolomonAddress(false);
         const { aliases } = await signum.service.query<AliasList>('getAliases', { account: accountId });
+        // TODO: this legacy pattern is deprecated
         const aliasPattern = new RegExp(`acct:(s|ts|burst)-${rs.toLowerCase()}@burst`);
-        const accountsAliases = aliases.filter(({ aliasURI }) => aliasPattern.test(aliasURI));
+        const accountsAliases = aliases.filter(({ aliasURI }) => {
+          try {
+            // checking for SRC44 spec
+            const json = JSON.parse(aliasURI);
+            return json.ac === accountId;
+          } catch (e) {
+            return aliasPattern.test(aliasURI);
+          }
+        });
         // Aliases are ordered by times - newest is last
         return accountsAliases.length ? accountsAliases[accountsAliases.length - 1].aliasName : null;
       } catch (e) {
@@ -35,14 +67,14 @@ export function useSignumAliasResolver() {
         if (aliasName.length < 2) {
           return null;
         }
-        const { account } = await signum.alias.getAliasByName(aliasName);
+        const { account, aliasURI } = await signum.alias.getAliasByName(aliasName);
+        const accountId = tryGetAccountIdFromAliasContent(aliasURI);
+        if (!accountId) return null;
         const acc = await signum.account.getAccount({
           accountId: account,
           includeEstimatedCommitment: false,
           includeCommittedAmount: false
         });
-        // Account structure of SignumJS is inconsistent
-        // @ts-ignore
         return acc.publicKey;
       } catch (e) {
         return null;
