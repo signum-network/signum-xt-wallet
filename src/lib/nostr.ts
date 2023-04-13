afc815ac8e9b0f55454a55d16345ead1fd74ebef
ohager <oliver@devbutze.com>
2023-04-13 14:02:47 -0300

    feat: hardening nostr key generation using pbkdf2

--- src/lib/nostr.ts
+++ src/lib/nostr.ts
@@ -2,21 +2,51 @@ import * as secp256k1 from '@noble/secp256k1';
 import { nip19 } from 'nostr-tools';
 
 import { shortenString } from 'lib/shortenString';
+
 export interface NostrKeys {
   publicKey: string;
   privateKey: string;
 }
 
-async function sha512(message: string) {
+/*
+ * Used to derives a key (adding more entropy) from the initial seed
+ * The steps are:
+ * - sha256 from the initial seed as salt
+ * - Apply PBKDF2 using the derivation parameters
+ * - export key and convert to UInt8Array
+ * - finally use as privatekey generation input
+ */
+
+const KeyDerivationParameters = {
+  iterations: 500_000,
+  hash: 'SHA-256',
+  keyType: { name: 'HMAC', hash: 'SHA-512', length: 512 }
+};
+
+async function deriveKey(seed: string, params = KeyDerivationParameters) {
   const encoder = new TextEncoder();
-  const data = encoder.encode(message);
-  const hashBuffer = await crypto.subtle.digest('SHA-512', data);
-  return secp256k1.utils.bytesToHex(new Uint8Array(hashBuffer));
+  const data = encoder.encode(seed);
+  const salt = await crypto.subtle.digest('SHA-256', data);
+  const keyMaterial = await crypto.subtle.importKey('raw', encoder.encode(seed), 'PBKDF2', false, ['deriveKey']);
+  const derivedKey = await crypto.subtle.deriveKey(
+    {
+      name: 'PBKDF2',
+      salt,
+      iterations: params.iterations,
+      hash: params.hash
+    },
+    keyMaterial,
+    params.keyType,
+    true,
+    ['sign']
+  );
+
+  return crypto.subtle.exportKey('raw', derivedKey);
 }
 
 export async function generateNostrKeys(seed: string): Promise<NostrKeys> {
-  const hashedSeed = await sha512(seed);
-  const privateKeyBytes = secp256k1.utils.hashToPrivateKey(hashedSeed);
+  const derivedKey = await deriveKey(seed);
+  const privateKeyBytes = secp256k1.utils.hashToPrivateKey(new Uint8Array(derivedKey));
   const privateKey = secp256k1.utils.bytesToHex(privateKeyBytes);
   const publicKey = secp256k1.utils.bytesToHex(secp256k1.schnorr.getPublicKey(privateKey));
   return {
